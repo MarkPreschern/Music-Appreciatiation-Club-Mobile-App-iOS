9b853b97077ee2e4eaf2ac393d3ab09547f5857b
Mark Preschern <mark.preschern@gmail.com>
2019-04-27 19:12:05 -0400

    database now has 1 'item' table representing either a song or album'

--- database/music_appreciation_club_database.sql
+++ database/music_appreciation_club_database.sql
@@ -2,9 +2,6 @@
 -- Schema music_appreciation_club
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `music_appreciation_club` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema music_appreciation_club
--- -----------------------------------------------------
 USE `music_appreciation_club` ;
 
 -- -----------------------------------------------------
@@ -22,35 +19,15 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `music_appreciation_club`.`album`
+-- Table `music_appreciation_club`.`item`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`album` (
-  `album_id` INT NOT NULL AUTO_INCREMENT,
-  `album_name` VARCHAR(100) NOT NULL,
-  `album_artist` VARCHAR(100) NOT NULL,
-  `album_image` BLOB NULL,
-  PRIMARY KEY (`album_id`),
-  UNIQUE INDEX `album_id_UNIQUE` (`album_id` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `music_appreciation_club`.`song`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`song` (
-  `song_id` INT NOT NULL AUTO_INCREMENT,
-  `song_name` VARCHAR(100) NOT NULL,
-  `song_artist` VARCHAR(100) NOT NULL,
-  `song_image` BLOB NULL,
-  `album_id` INT NULL,
-  PRIMARY KEY (`song_id`),
-  UNIQUE INDEX `song_id_UNIQUE` (`song_id` ASC) VISIBLE,
-  INDEX `fk_song_id_album_id1_idx` (`album_id` ASC) VISIBLE,
-  CONSTRAINT `fk_song_id_album_id1`
-    FOREIGN KEY (`album_id`)
-    REFERENCES `music_appreciation_club`.`album` (`album_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`item` (
+  `item_id` INT NOT NULL AUTO_INCREMENT,
+  `is_album` TINYINT NOT NULL,
+  `item_name` VARCHAR(255) NOT NULL,
+  `item_artist` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  UNIQUE INDEX `item_id_UNIQUE` (`item_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -61,36 +38,21 @@ CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`favorite` (
   `favorite_id` INT NOT NULL AUTO_INCREMENT,
   `date_favorited` DATETIME NOT NULL,
   `user_id` INT NOT NULL,
-  `song_id` INT NULL,
-  `album_id` INT NULL,
-  PRIMARY KEY (`favorite_id`),
+  `item_id` INT NOT NULL,
+  PRIMARY KEY (`favorite_id`, `user_id`, `item_id`),
   UNIQUE INDEX `favor_UNIQUE` (`favorite_id` ASC) VISIBLE,
-  INDEX `fk_favorite_id_user_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_favorite_id_song_id1_idx` (`song_id` ASC) VISIBLE,
-  INDEX `fk_favorite_id_album_id1_idx` (`album_id` ASC) VISIBLE,
-  CONSTRAINT `fk_favorite_id_user`
+  INDEX `fk_favorite_user1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_favorite_item2_idx` (`item_id` ASC) VISIBLE,
+  CONSTRAINT `fk_favorite_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `music_appreciation_club`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_favorite_id_song_id1`
-    FOREIGN KEY (`song_id`)
-    REFERENCES `music_appreciation_club`.`song` (`song_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_favorite_id_album_id1`
-    FOREIGN KEY (`album_id`)
-    REFERENCES `music_appreciation_club`.`album` (`album_id`)
+  CONSTRAINT `fk_favorite_item2`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `music_appreciation_club`.`item` (`item_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `music_appreciation_club`.`weekly_favorites`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`weekly_favorites` (
-)
 ENGINE = InnoDB;
 
 
@@ -101,26 +63,19 @@ CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`favorite_weekly` (
   `favorite_weekly_id` INT NOT NULL AUTO_INCREMENT,
   `date_favorited` DATETIME NOT NULL,
   `user_id` INT NOT NULL,
-  `song_id` INT NULL,
-  `album_id` INT NULL,
-  PRIMARY KEY (`favorite_weekly_id`),
+  `item_id` INT NOT NULL,
+  PRIMARY KEY (`favorite_weekly_id`, `user_id`, `item_id`),
   UNIQUE INDEX `favorite_weekly_id_UNIQUE` (`favorite_weekly_id` ASC) VISIBLE,
-  INDEX `fk_favorite_weekly_id_user1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_favorite_weekly_id_song_id1_idx` (`song_id` ASC) VISIBLE,
-  INDEX `fk_favorite_weekly_id_album_id1_idx` (`album_id` ASC) VISIBLE,
-  CONSTRAINT `fk_favorite_weekly_id_user1`
+  INDEX `fk_favorite_weekly_user1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_favorite_weekly_item1_idx` (`item_id` ASC) VISIBLE,
+  CONSTRAINT `fk_favorite_weekly_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `music_appreciation_club`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_favorite_weekly_id_song_id1`
-    FOREIGN KEY (`song_id`)
-    REFERENCES `music_appreciation_club`.`song` (`song_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_favorite_weekly_id_album_id1`
-    FOREIGN KEY (`album_id`)
-    REFERENCES `music_appreciation_club`.`album` (`album_id`)
+  CONSTRAINT `fk_favorite_weekly_item1`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `music_appreciation_club`.`item` (`item_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -139,19 +94,19 @@ CREATE TABLE IF NOT EXISTS `music_appreciation_club`.`vote` (
   PRIMARY KEY (`vote_id`, `user_id`, `favorite_id`, `favorite_weekly_id`),
   INDEX `fk_vote_user1_idx` (`user_id` ASC) VISIBLE,
   UNIQUE INDEX `vote_id_UNIQUE` (`vote_id` ASC) VISIBLE,
-  INDEX `fk_vote_favorite_id1_idx` (`favorite_id` ASC) VISIBLE,
-  INDEX `fk_vote_favorite_weekly_id1_idx` (`favorite_weekly_id` ASC) VISIBLE,
+  INDEX `fk_vote_favorite1_idx` (`favorite_id` ASC) VISIBLE,
+  INDEX `fk_vote_favorite_weekly1_idx` (`favorite_weekly_id` ASC) VISIBLE,
   CONSTRAINT `fk_vote_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `music_appreciation_club`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_vote_favorite_id1`
+  CONSTRAINT `fk_vote_favorite1`
     FOREIGN KEY (`favorite_id`)
     REFERENCES `music_appreciation_club`.`favorite` (`favorite_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_vote_favorite_weekly_id1`
+  CONSTRAINT `fk_vote_favorite_weekly1`
     FOREIGN KEY (`favorite_weekly_id`)
     REFERENCES `music_appreciation_club`.`favorite_weekly` (`favorite_weekly_id`)
     ON DELETE NO ACTION
